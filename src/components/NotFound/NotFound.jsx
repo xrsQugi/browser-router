e5c60fc6f1d2ee2770659e0a7df99ddb50a1e2da
xrsQugi <02.04.5.art@gmail.com>
2023-07-27 17:54:43 +0300

    page not found

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
-// import css from './NotFound.module.css';
+import css from './NotFound.module.css';
 
 export default function NotFound() {
     return(
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div className={css.block}>
+            <h1 className={css.title}>404</h1>
+            {/* <img src="https://static.vecteezy.com/system/resources/previews/015/131/133/original/crossing-sign-error-404-png.png" alt="404" width="300" /> */}
+            <p className={css.text}>Page not found</p>
         </div>
     )
 };
